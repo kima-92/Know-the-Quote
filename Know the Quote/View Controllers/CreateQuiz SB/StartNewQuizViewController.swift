557badaeee13624cc8e4aacdcdef5518aae7bf3a
Wilma <kima_92@fastmail.com>
2020-11-03 11:54:07 -0500

    Creating the CreateQuiz Storyboard

---
+++ Know the Quote/View Controllers/CreateQuiz SB/StartNewQuizViewController.swift
@@ -0,0 +1,29 @@
+//
+//  StartNewQuizViewController.swift
+//  Know the Quote
+//
+//  Created by macbook on 11/3/20.
+//
+
+import UIKit
+
+class StartNewQuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
