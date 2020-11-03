e647eb5094f85414098a1ff87b055d63f7638380
Wilma <kima_92@fastmail.com>
2020-11-03 12:50:10 -0500

    Adding CoreData to the project

---
+++ Know the Quote/Controllers/Model Controllers/QuizController.swift
@@ -0,0 +1,16 @@
+//
+//  QuizController.swift
+//  Know the Quote
+//
+//  Created by macbook on 11/3/20.
+//
+
+import Foundation
+import CoreData
+
+struct QuizController {
+    
+    func createQuiz(title: String, quotes: [Quote], creatorID: UUID) {
+        let quiz = Quiz(title: title, creatorID: creatorID, quotes: quotes)
+    }
+}
