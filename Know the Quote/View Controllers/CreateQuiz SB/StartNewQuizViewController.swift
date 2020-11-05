d2e2ba19269c5a0ffd315311673ad8eb5782f687
Wilma <kima_92@fastmail.com>
2020-11-04 23:23:48 -0500

    Adding textFields and a button in UI to start gathering info for the new Quiz

--- Know the Quote/View Controllers/CreateQuiz SB/StartNewQuizViewController.swift
+++ Know the Quote/View Controllers/CreateQuiz SB/StartNewQuizViewController.swift
@@ -8,22 +8,19 @@
 import UIKit
 
 class StartNewQuizViewController: UIViewController {
-
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var numOfQuotesTextField: UITextField!
+    
+    // MARK: - ViewDidLoad
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Actions
+    
+    @IBAction func startButtonTapped(_ sender: UIButton) {
     }
-    */
-
 }
