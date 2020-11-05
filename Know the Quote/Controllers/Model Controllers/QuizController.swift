4f03048ad695a163e779a1e411856584369bd740
Wilma <kima_92@fastmail.com>
2020-11-04 23:10:42 -0500

    Creating functionality in the QuizController to create Quotes and Quizzes

--- Know the Quote/Controllers/Model Controllers/QuizController.swift
+++ Know the Quote/Controllers/Model Controllers/QuizController.swift
@@ -8,10 +8,42 @@
 import Foundation
 import CoreData
 
-struct QuizController {
+class QuizController {
     
-    func createQuiz(title: String, quotes: [Quote], creatorID: UUID) {
-        //let quiz = 
+    // MARK: - Properties
+    var quotes: [Quote] = []
+    
+    // MARK: - Methods
+    
+    // Create new Quiz and save in CD
+    func createQuiz(title: String, creator: User, context: NSManagedObjectContext) {
         
+        // If there's more than 3 quotes, create the quiz and add it to it's creator's quizesCreated.
+        if quotes.count > 3 {
+            
+            let quiz = Quiz(title: title, creator: creator, context: context)
+            
+            for quote in quotes {
+                quiz.addToQuotes(quote)
+            }
+            creator.addToQuizesCreated(quiz)
+            CoreDataStack.shared.save(context: context)
+            
+        } else {
+            // TODO: - Alert the user they need to add at least 3 Quotes to save the new Quiz
+        }
+    }
+    
+    // Add a new quote to the array
+    func createQuote(firstPart: String = "", secondPart: String = "", incorrectAnswers: [String], correctAnswer: String, context: NSManagedObjectContext) {
+        
+        // If there's less than 16 quotes in the array, create this new quote and add it to the array (NOT saved in CD yet)
+        if quotes.count < 16 {
+            let quote = Quote(firstPart: firstPart, secondPart: secondPart, incorrectAnswers: incorrectAnswers, correctAnswer: correctAnswer, context: context)
+            
+            self.quotes.append(quote)
+        } else {
+            // TODO: - Alert the user they can't create a Quiz with more than 15 Quotes
+        }
     }
 }
