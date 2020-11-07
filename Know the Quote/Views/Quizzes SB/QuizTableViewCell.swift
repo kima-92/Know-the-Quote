d585c0ab59746f1e9098a1c0efb2ee64309946e5
Wilma <kima_92@fastmail.com>
2020-11-07 17:11:07 -0500

    Creating the QuizTableViewCell

---
+++ Know the Quote/Views/Quizzes SB/QuizTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  QuizTableViewCell.swift
+//  Know the Quote
+//
+//  Created by macbook on 11/7/20.
+//
+
+import UIKit
+
+class QuizTableViewCell: UITableViewCell {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var quoteCountLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
