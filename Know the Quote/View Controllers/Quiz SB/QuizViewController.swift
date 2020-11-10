2cdf0f78e2a4f139b341a040f7d0b7e08c1370f8
Wilma <kima_92@fastmail.com>
2020-11-09 21:34:09 -0500

    Wiring buttons to UI for QuoteVC

--- Know the Quote/View Controllers/Quiz SB/QuizViewController.swift
+++ Know the Quote/View Controllers/Quiz SB/QuizViewController.swift
@@ -41,14 +41,16 @@ class QuizViewController: UIViewController {
         titleLabel.text = quiz.title
     }
     
-    /*
     // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "StartQuizSegue" {
+            guard let quoteVC = segue.destination as? QuoteViewController else { return }
+            
+            quoteVC.user = user
+            quoteVC.quizController = quizController
+            quoteVC.quiz = quiz
+        }
     }
-    */
-
 }
--- Know the Quote/View Controllers/Quiz SB/QuoteViewController.swift
+++ Know the Quote/View Controllers/Quiz SB/QuoteViewController.swift
@@ -8,22 +8,71 @@
 import UIKit
 
 class QuoteViewController: UIViewController {
-
+    
+    // MARK: - Properties
+    
+    var quizController: QuizController?
+    var user: User?
+    var quiz: Quiz?
+    var buttons: [UIButton?] = []
+    
+    // MARK: - Outlets
+    
+    @IBOutlet weak var quoteLabel: UILabel!
+    
+    @IBOutlet weak var opt1Button: UIButton!
+    @IBOutlet weak var opt2Button: UIButton!
+    @IBOutlet weak var opt3Button: UIButton!
+    @IBOutlet weak var opt4Button: UIButton!
+    @IBOutlet weak var opt5Button: UIButton!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    // MARK: - DidLoad
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        gatherButtons()
+        updateViews()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    // MARK: - Methods
+    
+    private func gatherButtons() {
+        buttons.append(opt1Button)
+        buttons.append(opt2Button)
+        buttons.append(opt3Button)
+        buttons.append(opt4Button)
+        buttons.append(opt5Button)
     }
-    */
-
+    
+    private func displayQuote() {
+//        guard let quizController = quizController,
+//              let quiz = quiz else { return }
+//
+//        quizController.getAllQuotes(quiz: quiz)
+//        quoteLabel.text = quizController.getCurrentQuoteText()
+    }
+    
+    private func setAllOptions() {
+//        guard let quizController = quizController,
+//              let quote = quizController.currentQuote1 else { return }
+//
+//        if let options = quote.allOptions as? [String] {
+//
+//            for option in options {
+//                for button in buttons {
+//                    button?.setTitle(option, for: .normal)
+//                }
+//            }
+//        }
+    }
+    
+    private func updateViews() {
+        guard let quizController = quizController,
+              let quiz = quiz else { return }
+        
+        displayQuote()
+        setAllOptions()
+    }
 }
